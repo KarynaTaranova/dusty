cdea96d1f75ff7eb607534b552276fb22ae40745
Ivan Krakhmaliuk <lifedj27@gmail.com>
2019-07-03 10:16:24 +0300

    Try inscriptis

--- dusty/scanners/dast/zap/parser.py
+++ dusty/scanners/dast/zap/parser.py
@@ -22,7 +22,9 @@
 
 import json
 
-from dusty.tools import log, markdown
+import inscriptis
+
+from dusty.tools import log
 from dusty.models.finding import DastFinding
 
 from . import constants
@@ -36,16 +38,16 @@ def parse_findings(data, scanner):
         for alert in site["alerts"]:
             description = list()
             if "desc" in alert:
-                description.append(markdown.html_to_markdown(alert["desc"]))
+                description.append(inscriptis.get_text(alert["desc"]))
             if "solution" in alert:
                 description.append(
-                    f'**Solution:**\n {markdown.html_to_markdown(alert["solution"])}')
+                    f'**Solution:**\n {inscriptis.get_text(alert["solution"])}')
             if "reference" in alert:
                 description.append(
-                    f'**Reference:**\n {markdown.html_to_markdown(alert["reference"], True)}')
+                    f'**Reference:**\n {inscriptis.get_text(alert["reference"], True)}')
             if "otherinfo" in alert:
                 description.append(
-                    f'**Other information:**\n {markdown.html_to_markdown(alert["otherinfo"])}')
+                    f'**Other information:**\n {inscriptis.get_text(alert["otherinfo"])}')
             description = "\n".join(description)
             # Make finding object
             finding = DastFinding(
