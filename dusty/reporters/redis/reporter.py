9f83408fd4fcaa326ef3014b7f86df8d2cca46b8
Ivan Krakhmaliuk <lifedj27@gmail.com>
2019-07-05 17:00:45 +0300

    Style fix

--- dusty/reporters/redis/reporter.py
+++ dusty/reporters/redis/reporter.py
@@ -52,7 +52,8 @@ class Reporter(DependentModuleModel, ReporterModel):
         """ Make sample config """
         data_obj.insert(
             len(data_obj),
-            "connection_string", "redis://redis.example.com:6379/0", comment="Redis connection string"
+            "connection_string", "redis://redis.example.com:6379/0",
+            comment="Redis connection string"
         )
 
     @staticmethod
