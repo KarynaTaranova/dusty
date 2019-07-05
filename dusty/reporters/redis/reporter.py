0261b3422f0a25f81ec91a4425a29e1e177df101
Ivan Krakhmaliuk <lifedj27@gmail.com>
2019-07-05 15:47:55 +0300

    Redis: added redis reporter

---
+++ dusty/reporters/redis/reporter.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+# coding=utf-8
+# pylint: disable=I0011,E0401
+
+#   Copyright 2019 getcarrier.io
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""
+    Reporter: redis
+"""
+
+from dusty.tools import log
+from dusty.models.module import DependentModuleModel
+from dusty.models.reporter import ReporterModel
+
+from .legacy import RedisFile
+
+
+class Reporter(DependentModuleModel, ReporterModel):
+    """ Report findings from scanners """
+
+    def __init__(self, context):
+        """ Initialize reporter instance """
+        super().__init__()
+        self.context = context
+        self.config = \
+            self.context.config["reporters"][__name__.split(".")[-2]]
+
+    def report(self):
+        """ Report """
+        # Prepare wrapper
+        log.info("Saving HTML/XML reports (if any) to redis using legacy wrapper")
+        RedisFile(
+            self.config.get("connection_string"),
+            self.context.performers["reporting"].get_module_meta("html", "report_file", None),
+            self.context.performers["reporting"].get_module_meta("junit", "report_file", None)
+        )
+
+    @staticmethod
+    def fill_config(data_obj):
+        """ Make sample config """
+        data_obj.insert(
+            len(data_obj),
+            "connection_string", "redis.example.com", comment="Redis connection string"
+        )
+
+    @staticmethod
+    def validate_config(config):
+        """ Validate config """
+        if "connection_string" not in config:
+            error = "No Redis connection string defined in config"
+            log.error(error)
+            raise ValueError(error)
+
+    @staticmethod
+    def get_name():
+        """ Reporter name """
+        return "Redis"
+
+    @staticmethod
+    def get_description():
+        """ Reporter description """
+        return "Redis reporter"
