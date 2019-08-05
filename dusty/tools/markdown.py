a7b8e37c1dd25679c2abc27b80fccb6062595db4
Ivan Krakhmaliuk <lifedj27@gmail.com>
2019-08-05 11:54:52 +0300

    Core: markdown: also html escape blocks

--- dusty/tools/markdown.py
+++ dusty/tools/markdown.py
@@ -21,6 +21,7 @@
 """
 
 import re
+import html
 import markdown2
 import inscriptis
 
@@ -106,8 +107,10 @@ def _markdown2_postprocess(self, text):  # pylint: disable=W0613
     return text
 
 
-def markdown_escape(string):
+def markdown_escape(string, html_escape=True):
     """ Escape markdown special symbols """
+    if html_escape:
+        string = html.escape(string)
     to_escape = [
         "\\", "`", "*", "_",
         "{", "}", "[", "]", "(", ")",
