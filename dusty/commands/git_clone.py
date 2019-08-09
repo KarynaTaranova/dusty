5529ddd7aada0fbb936cc5d8fcbb5d00562892a0
Ivan Krakhmaliuk <lifedj27@gmail.com>
2019-08-09 11:12:33 +0300

    Git: SSH key data debug

--- dusty/commands/git_clone.py
+++ dusty/commands/git_clone.py
@@ -134,10 +134,8 @@ class Command(ModuleModel, CommandModel):
             auth_args["key_filename"] = os.environ[args.key_variable]
         if args.key_data_variable and args.key_data_variable in os.environ:
             key_file_fd, key_file_path = tempfile.mkstemp()
-            os.write(key_file_fd, os.environ[args.key_data_variable])
+            os.write(key_file_fd, os.environ[args.key_data_variable].encode("utf-8"))
             os.close(key_file_fd)
-            log.debug("Key file: %s", key_file_path)
-            log.debug("Key value: %s", os.environ[args.key_data_variable])
             auth_args["key_filename"] = key_file_path
         # Take from commandline parameters
         if args.username:
